9398d858cd2ed27664c74a57abd08570639b9ddf
ZarinaKaytukova <147167938+ZarinaKaytukova@users.noreply.github.com>
2026-03-30 23:50:19 +0300

    Update config.cjs

--- app/config/config.cjs
+++ app/config/config.cjs
@@ -2,12 +2,8 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    username: process.env.DATABASE_USERNAME || 'postgres',
-    password: process.env.DATABASE_PASSWORD || 'password',
-    database: process.env.DATABASE_NAME || 'postgres',
-    host: process.env.DATABASE_HOST || 'db',
-    port: process.env.DATABASE_PORT || 5432,
-    dialect: 'postgres',
+    dialect: 'sqlite',
+    storage: './database.sqlite',
     logging: false,
   },
   test: {
